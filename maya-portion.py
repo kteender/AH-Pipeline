0ac576e09c1bf7a0e7aabbaeb5a703416c779bdb
Katie Tender <kttender@gmail.com>
2022-06-14 21:27:19 -0700

    getting range from time slider

--- maya-portion.py
+++ maya-portion.py
@@ -1,3 +1,11 @@
 import maya.cmds as cmds
 
-export 
+def get_timeline_start_stop():
+    start = cmds.playbackOptions(q=True, ast=True)
+    stop = cmds.playbackOptions(q=True, aet=True)
+    return start, stop
+
+def export_abc():
+    start, stop = get_timeline_start_stop()
+    #abc export mel command: AbcExport -j "-frameRange 1 200 -dataFormat ogawa -root |pCube1 -file D:/_CURRENT/AH/AH_proj/cache/alembic/cube.abc";
+    return
