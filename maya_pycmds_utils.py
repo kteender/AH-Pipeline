01e4ba970c565ddfb206ce79b8d94ac804608464
Katie Tender <kttender@gmail.com>
2022-06-25 11:07:18 -0700

    some changes?

--- maya_pycmds_utils.py
+++ maya_pycmds_utils.py
@@ -1,4 +1,4 @@
-import maya.cmds as cmds
+import maya.cmds as mc
 import logging
 
 thisLogger = logging.getLogger("MayaPyCmdsUtilsLogger")
@@ -8,22 +8,22 @@ def get_mat_on_object(shape):
     """should not be passed the object itself, should be passed its shape node
         eg cmds.ls(sl=True, dag=True, s=True)
         returns list of all materials"""
-    se = cmds.listConnections(shape, type='shadingEngine')
-    mat = cmds.ls(cmds.listConnections(se), materials=True)
+    se = mc.listConnections(shape, type='shadingEngine')
+    mat = mc.ls(mc.listConnections(se), materials=True)
     return mat
 
 def get_timeline_start_stop():
     """returns start and stop of main time slider as floats"""
-    start = cmds.playbackOptions(q=True, ast=True)
-    stop = cmds.playbackOptions(q=True, aet=True)
+    start = mc.playbackOptions(q=True, ast=True)
+    stop = mc.playbackOptions(q=True, aet=True)
     return start, stop
 
 def get_selected_objects_dag():
-    sels = cmds.ls(sl=True, dag=True, s=True)
+    sels = mc.ls(sl=True, dag=True, s=True)
     return sels
 
 def get_transform_for_shape(shape):
-    tr = cmds.listRelatives(shape, type='transform', p=True)
+    tr = mc.listRelatives(shape, type='transform', p=True)
     return tr
 
 def filter_out_orig_shapes(shapeList):
@@ -32,4 +32,3 @@ def filter_out_orig_shapes(shapeList):
             shapeList.remove(shape)
     return shapeList
 
-#def get_object_full_namepath(obj):
