fdc9100f9e55695c5728b30993bd3029bb2e61b7
Katie Tender <kttender@gmail.com>
2022-07-10 11:00:44 -0700

    now logs to blender console. Apply tx properly

--- blender_portion.py
+++ blender_portion.py
@@ -1,24 +1,42 @@
 import sys
 import argparse
 import logging
-import time
 import json
 import imp
+import mathutils
+import subprocess
+import os
 
 import bpy
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+target = os.path.join(sys.prefix, 'lib', 'site-packages')
+print("python exe is")
+print(python_exe)
 sys.path.append("D:\\_CURRENT\\AH\\AH_pipeline\\")
 import blender_py_utils as utils
-
 imp.reload(utils)
 
-thisLogger = logging.getLogger("BlenderScriptLogger")
+try:
+    from PIL import Image
+except:
+    sys.path.append("C:\\Users\\Clown Car\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python310\\site-packages\\")
+    from PIL import Image
+
+#thisLogger = logging.getLogger("BlenderScriptLogger")
+bLogger = logging.getLogger(__name__)
+thisLogger = logging.getLogger(__name__+' Blender Portion Logger')
 thisLogger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+fm = logging.Formatter(fmt=' %(name)s : %(levelname)-8s : %(message)s')
+sh.setFormatter(fm)
+thisLogger.addHandler(sh)
 
 HIGHLIGHT_NAMES = ["rig:margo_bun_geo", "rig:margo_hair_geo"]
-NOHIGHLIGHT_NAMES = ["rig:mouthHighResBase","rig:margo_body_geo"]
+NOHIGHLIGHT_NAMES = ["rig:margo_body_geo"]
 EYE_NAMES = ['rig:eyeLeft_geo', 'rig:eyeRight_geo']
 EYEMASK_NAMES = ["rig:eyeLeft_geo_mask", "rig:eyeRight_geo_mask"]
-FACE_NAMES = ["rig:mouthHighResBase", "rig:noseLeft_geo"]
+FACE_NAMES = ["rig:mouth_geo", "rig:noseLeft_geo"]
 
 CHANNEL_LIST = ['color']
 
@@ -88,6 +106,43 @@ def sort_objects():
             else:
                 continue
 
+def create_material_from_dict(nm, d, *args):
+    print("d is")
+    print(d)
+    thisLogger.critical("testing")
+    mat = bpy.data.materials.new(nm)
+    mat.use_nodes = True
+    nt = mat.node_tree
+    nt.nodes.remove(nt.nodes['Material Output'])
+    nt.nodes.remove(nt.nodes['Principled BSDF'])
+    mat.node_tree.links.clear()
+    moN = nt.nodes.new('ShaderNodeOutputMaterial')
+    shN = nt.nodes.new('ShaderNodeBsdfDiffuse')
+    mat.node_tree.links.new(shN.outputs['BSDF'], moN.inputs['Surface'])
+
+    for a in args:
+        print("arg is %s" % a)
+        if a  == 'color':
+            k = nm+ ".color"
+            col = d[k]
+            print("col is")
+            print(col)
+            if isinstance(col, str):
+                size = (1000,1000)
+                with Image.open(col) as im:
+                    size = im.size
+                size = list(size)
+                txN = nt.nodes.new('ShaderNodeTexImage')
+                txI = bpy.data.images.new(col, width=size[0], height=size[1])
+                txI.source = 'FILE'
+                txI.filepath = col
+                txN.image = txI
+                mat.node_tree.links.new(txN.outputs['Color'], shN.inputs['Color'])
+            else:
+                bcol = mathutils.Color(col)
+                shN.color = bcol
+    return 
+
 def apply_materials(path):
     f = open(path)
     matData = json.load(f)
@@ -101,8 +156,10 @@ def apply_materials(path):
                 print("mat is")
                 print(type(mat))
                 print(mat)
-                m = utils.create_material_from_dict(mat, matData[obj][mat], *CHANNEL_LIST)
-            utils.apply_material_to_object(obj, m)
+                m = create_material_from_dict(mat, matData[obj][mat], *CHANNEL_LIST)
+            tr = obj.replace("Shape", "")
+            if tr in bpy.data.objects:
+                utils.apply_material_to_object(bpy.data.objects[tr], m)
             
 
 
